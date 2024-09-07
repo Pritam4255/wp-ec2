25c8409508a7d5e2a2af2181e2263adccbc98e2e
Pritam4255 <pritam4255@gmail.com>
2024-09-07 16:30:50 +0530

    Create wp-download.sh

---
+++ wp-download.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+apt update
+apt upgrade -y
+apt install nginex
+apt install wget
+cd /var/www
+wget https://wordpress.org/latest.tar.gz
+sudo tar -xzvf latest.tar.gz
+rm -rf latest.tar.gz
+sudo chown -R www-data:www-data wordpress
+sudo find wordpress/ -type d -exec chmod 755 {} \;
+sudo find wordpress/ -type f -exec chmod 644 {} \;
+
